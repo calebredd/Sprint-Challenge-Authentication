bc67a6cb339a0deadf324158f0eeff42acd488cf
Caleb Redd <caleb.redd@yahoo.com>
2019-12-15 22:11:05 -0700

    Uncommented out list of jokes from react app

--- api/client/src/App.js
+++ api/client/src/App.js
@@ -6,20 +6,20 @@ import Jokes from "./components/Jokes.js";
 function App() {
   const [jokes, setJokes]=useState([]);
   useEffect(()=>{
-    axios.post('/api/auth/login',{header:{"username":"Mikey","password":"password"}}).then(res=>{
-      console.log(res.data);
-    }).catch(err=>{
-      console.error(err);
-    })
-    // axios
-    // .get('/api/jokes',{headers:{"authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOjMsInVzZXJuYW1lIjoiTWlrZXkiLCJpYXQiOjE1NzY0NzE2MzcsImV4cCI6MTU3NjQ3NTIzN30.lxUe-Rl7hYl8Oih6cA9THc61pI_xvRrSgKgb0y_ueeA"}})
-    // .then(res=>{
+    // axios.post('/api/auth/login',{headers:{"username":"Mikey","password":"password"}}).then(res=>{
     //   console.log(res.data);
-    //   setJokes(res.data);
-    // })
-    // .catch(err=>{
+    // }).catch(err=>{
     //   console.error(err);
-    // });
+    // })
+    axios
+    .get('/api/jokes',{headers:{"authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOjMsInVzZXJuYW1lIjoiTWlrZXkiLCJpYXQiOjE1NzY0NzE2MzcsImV4cCI6MTU3NjQ3NTIzN30.lxUe-Rl7hYl8Oih6cA9THc61pI_xvRrSgKgb0y_ueeA"}})
+    .then(res=>{
+      console.log(res.data);
+      setJokes(res.data);
+    })
+    .catch(err=>{
+      console.error(err);
+    });
   },[]);
 // console.log(jokes);
   return (
